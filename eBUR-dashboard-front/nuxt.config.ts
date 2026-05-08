f193a0cbb3ced22dc5aa7286c5387492b03fbfd6
Егор Чижов <elshmelle@ya.ru>
2026-05-08 13:49:57 +0500

    Добавлены зависимости
    gridstack и date-fns добавлены в зависимости для оптимизации Vite

---
+++ eBUR-dashboard-front/nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@vueuse/nuxt',
+  ],
+
+  vite: {
+    optimizeDeps: {
+      include: [
+        'date-fns', //tmp
+        'gridstack',
+      ],
+    },
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/api/**': {
+      cors: true
+    }
+  },
+
+  devtools: {
+    enabled: true
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
