2fe5bf91d54db66d142a4aade0a0d75e5721fb15
Егор Чижов <elshmelle@ya.ru>
2026-06-11 15:37:27 +0500

    Добавлена локализация
    Локализация главной страницы для интеграции с основным проектом.

--- eBUR-dashboard-front/nuxt.config.ts
+++ eBUR-dashboard-front/nuxt.config.ts
@@ -4,7 +4,21 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui',
     '@vueuse/nuxt',
+    '@nuxtjs/i18n',
   ],
+
+  i18n: {
+    locales: [
+      { code: 'ru', language: 'ru-RU', file: 'ru-RU.json' },
+      { code: 'en', language: 'en-US', file: 'en-US.json' }
+    ],
+    detectBrowserLanguage: {
+      useCookie: false,
+      redirectOn: 'root'
+    },
+    // strategy: 'no_prefix',
+    defaultLocale: 'ru-RU'
+  },
 
   vite: {
     optimizeDeps: {
